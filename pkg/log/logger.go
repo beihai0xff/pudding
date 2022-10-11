d0b913adbc0bf0c6387bc9a49611ab972dec4217
beihai <beihai@wingsxdu.com>
2022-10-11 22:38:59 +0800

    优化 redis broker 逻辑

--- pkg/log/logger.go
+++ pkg/log/logger.go
@@ -3,6 +3,7 @@ package log
 import (
 	"os"
 
+	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -30,22 +31,30 @@ func newZapLogWithCallerSkip(c *OutputConfig, callerSkip int) *zap.Logger {
 
 func newConsoleCore(c *OutputConfig) zapcore.Core {
 	level := zap.NewAtomicLevelAt(Levels[c.Level])
+	hook := lumberjack.Logger{
+		Filename:   "./log/pudding.log", // 日志文件路径
+		MaxSize:    128,                 // 每个日志文件保存的大小 单位:M
+		MaxAge:     7,                   // 文件最多保存多少天
+		MaxBackups: 30,                  // 日志文件最多保存多少个备份
+		Compress:   false,               // 是否压缩
+	}
+	var writes = []zapcore.WriteSyncer{zapcore.AddSync(&hook), zapcore.AddSync(os.Stdout)}
 	return zapcore.NewCore(
 		newEncoder(c),
-		zapcore.Lock(os.Stdout),
+		zapcore.NewMultiWriteSyncer(writes...),
 		level)
 }
 
 func newEncoder(c *OutputConfig) zapcore.Encoder {
 	encoderCfg := zapcore.EncoderConfig{
-		TimeKey:       "time",
-		LevelKey:      "level",
-		NameKey:       "name",
-		CallerKey:     "caller",
-		MessageKey:    "message",
-		StacktraceKey: "stacktrace",
-		LineEnding:    zapcore.DefaultLineEnding,
-		EncodeLevel:   zapcore.CapitalLevelEncoder,
+		TimeKey:    "time",
+		LevelKey:   "level",
+		NameKey:    "name",
+		CallerKey:  "caller",
+		MessageKey: "message",
+		// StacktraceKey: "stacktrace",
+		LineEnding:  zapcore.DefaultLineEnding,
+		EncodeLevel: zapcore.CapitalLevelEncoder,
 		// TODO: custom EncodeTime
 		EncodeTime:     zapcore.ISO8601TimeEncoder,
 		EncodeDuration: zapcore.StringDurationEncoder,
