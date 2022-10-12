176b8633f0dec02abb67fa37b60fa12565d47948
beihai <beihai@wingsxdu.com>
2022-10-12 17:15:23 +0800

    UnitTest: add redis zrange and hget test

--- pkg/redis/redis_test.go
+++ pkg/redis/redis_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"reflect"
 	"runtime"
+	"strconv"
 	"testing"
 	"time"
 
@@ -12,10 +14,37 @@ import (
 	"github.com/go-redis/redis/v9"
 )
 
+var (
+	key, value  = "GetKey", "GetValue"
+	table, zset = "hash_test", "zset_test"
+	c           *Client
+)
+
 // redis_test.go 测试文件对 Redis 客户端对外暴漏的方法进行了功能测试，连接的是 dev 环境的数据库 。
 // 下面的单元测试也可以作为使用范例参考
 
 func TestMain(m *testing.M) {
+	// initial Redis DB
+	s, _ := miniredis.Run()
+
+	s.ZAdd(zset, 1, "a")
+	s.ZAdd(zset, 2, "b")
+	s.ZAdd(zset, 3, "c")
+
+	s.HSet(table, key, value)
+
+	s.Set(key, value)
+	s.SetTTL("GetKey", 60*time.Second)
+
+	// initial Redis Client
+	c = &Client{
+		client: redis.NewClient(&redis.Options{
+			Addr:     s.Addr(),
+			DB:       0,
+			PoolSize: runtime.NumCPU() * 40,
+		}),
+	}
+
 	exitCode := m.Run()
 	// 退出
 	os.Exit(exitCode)
@@ -23,14 +52,6 @@ func TestMain(m *testing.M) {
 
 // TestClient_Set 测试 Set 方法
 func TestClient_Set(t *testing.T) {
-	s := miniredis.RunT(t)
-	c := &Client{
-		client: redis.NewClient(&redis.Options{
-			Addr:     s.Addr(),
-			DB:       0,
-			PoolSize: runtime.NumCPU() * 40,
-		}),
-	}
 
 	type args struct {
 		key        string
@@ -58,17 +79,6 @@ func TestClient_Set(t *testing.T) {
 
 // TestClient_Get 测试 Get 方法
 func TestClient_Get(t *testing.T) {
-	s := miniredis.RunT(t)
-	s.Set("GetKey", "GetValue")
-	s.SetTTL("GetKey", 60*time.Second)
-
-	c := &Client{
-		client: redis.NewClient(&redis.Options{
-			Addr:     s.Addr(),
-			DB:       0,
-			PoolSize: runtime.NumCPU() * 40,
-		}),
-	}
 
 	type args struct {
 		key string
@@ -79,8 +89,8 @@ func TestClient_Get(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{"Get 测试已经存储在 Redis的数据", args{"GetKey"}, "GetValue", false},
-		{"Get 测试未存储在 Redis的数据", args{"GetKey100"}, "", true},
+		{"Get 测试已存储在 Redis 的数据", args{"GetKey"}, "GetValue", false},
+		{"Get 测试未存储在 Redis 的数据", args{"GetKey100"}, "", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -98,15 +108,6 @@ func TestClient_Get(t *testing.T) {
 
 // TestClient_GetDistributeLock 测试 GetDistributeLock 方法
 func TestClient_GetDistributeLock(t *testing.T) {
-	s := miniredis.RunT(t)
-
-	c := &Client{
-		client: redis.NewClient(&redis.Options{
-			Addr:     s.Addr(),
-			DB:       0,
-			PoolSize: runtime.NumCPU() * 40,
-		}),
-	}
 
 	lock, _ := c.GetDistributeLock(context.Background(), "DLockUsed", 10*time.Second)
 	defer lock.Release(context.Background())
@@ -136,6 +137,68 @@ func TestClient_GetDistributeLock(t *testing.T) {
 			}
 			if tt.unlock {
 				mutex.Release(ctx)
+			}
+		})
+	}
+}
+
+func TestClient_HGet(t *testing.T) {
+
+	type args struct {
+		key   string
+		field string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{"HGet 测试已经存储在 Redis 的数据", args{table, key}, []byte("GetValue"), false},
+		{"HGet 测试未存储在 Redis 的数据", args{"no", key}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := c.HGet(context.Background(), tt.args.key, tt.args.field)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("HGet() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("HGet() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestClient_ZRangeByScore(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		key string
+		opt *redis.ZRangeBy
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []redis.Z
+		wantErr bool
+	}{
+		{"ZSet 获取已经存储在 Redis 的数据", args{context.Background(), zset, &redis.ZRangeBy{
+			Min:    "-inf",
+			Max:    strconv.FormatInt(time.Now().Unix(), 10),
+			Offset: 0,
+			Count:  10,
+		}}, []redis.Z{{1, "a"}, {2, "b"}, {3, "c"}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := c.ZRangeByScore(tt.args.ctx, tt.args.key, tt.args.opt)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ZRangeByScore() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ZRangeByScore() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
