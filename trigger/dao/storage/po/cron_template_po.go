7897eb05049123efcebbee7e51022d79b6417d19
beihai <beihai@wingsxdu.com>
2022-11-03 22:22:54 +0800

    feat: CronTemplate SQL unit test

--- trigger/dao/storage/po/cron_template_po.go
+++ trigger/dao/storage/po/cron_template_po.go
@@ -19,10 +19,10 @@ type CronTriggerTemplate struct {
 	Payload []byte `gorm:"column:payload;type:TEXT;not null;comment:'消息内容'" copier:"must,nopanic"`
 
 	// NextExecutionTime last time to schedule the message
-	LastExecutionTime time.Time `gorm:"column:last_execution_time;type:datetime;not null;comment:'上次执行时间'" copier:"must,nopanic"`
+	LastExecutionTime time.Time `gorm:"column:last_execution_time;type:TIMESTAMP;not null;comment:'上次执行时间'" copier:"must,nopanic"`
 
 	// ExceptedEndTime Excepted Trigger end time, if it is 0, it means that it will not end.
-	ExceptedEndTime time.Time `gorm:"column:excepted_end_time;type:datetime;not null;comment:'预期结束时间'" copier:"must,nopanic"`
+	ExceptedEndTime time.Time `gorm:"column:excepted_end_time;type:TIMESTAMP;not null;comment:'预期结束时间'" copier:"must,nopanic"`
 	// ExceptedLoopTimes except loop times
 	ExceptedLoopTimes uint64 `gorm:"column:excepted_loop_times;type:int unsigned;not null;default:0;comment:'预期循环次数'" copier:"must,nopanic"`
 	// LoopedTimes already loop times
