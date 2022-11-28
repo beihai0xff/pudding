9484468bc1220b8aa1a8b53f98b510d702ab33a0
beihai <beihai@wingsxdu.com>
2022-11-28 10:56:34 +0800

    improve: improve handler response and error (#108)

--- app/scheduler/handler.go
+++ app/scheduler/handler.go
@@ -2,15 +2,18 @@ package scheduler
 
 import (
 	"context"
+	"fmt"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	pb "github.com/beihai0xff/pudding/api/gen/pudding/scheduler/v1"
 	res "github.com/beihai0xff/pudding/api/gen/pudding/types/v1"
+	"github.com/beihai0xff/pudding/pkg/errno"
 	"github.com/beihai0xff/pudding/types"
 )
+
+const puddingScheduler = "pudding.scheduler"
 
 type Handler struct {
 	s Scheduler
@@ -18,24 +21,35 @@ type Handler struct {
 }
 
 func NewHandler(s Scheduler) *Handler {
-	return &Handler{s: s}
+	return &Handler{s: s, UnimplementedSchedulerServiceServer: pb.UnimplementedSchedulerServiceServer{}}
 }
 
 func (s *Handler) Ping(context.Context, *emptypb.Empty) (*res.PingResponse, error) {
 	return &res.PingResponse{Message: "pong"}, nil
 }
 
-func (s *Handler) SendDelayMessage(ctx context.Context, req *pb.SendDelayMessageRequest) (*res.Response,
-	error) {
+func (s *Handler) SendDelayMessage(ctx context.Context, req *pb.SendDelayMessageRequest) (*emptypb.Empty, error) {
 	msg := s.convPBToMessage(req)
-	if msg.DeliverAt == 0 && msg.DeliverAfter == 0 {
-		return nil, status.Errorf(codes.InvalidArgument, "DeliverAt and DeliverAfter can't be both zero")
+	if msg.DeliverAt <= 0 && msg.DeliverAfter <= 0 {
+		return nil, errno.BadRequest("deliver_at and deliver_after can't be both zero",
+			&errdetails.BadRequest_FieldViolation{
+				Field:       "deliver_after",
+				Description: fmt.Sprintf("deliver_after [%d] should be greater than zero", req.DeliverAfter),
+			},
+			&errdetails.BadRequest_FieldViolation{
+				Field:       "deliver_at",
+				Description: fmt.Sprintf("deliver_at [%d] should be greater than zero", req.DeliverAt),
+			})
 	}
 
 	if err := s.s.Produce(ctx, msg); err != nil {
-		return nil, status.Errorf(codes.Internal, err.Error())
+		return nil, errno.InternalError("can not produce message", &errdetails.ErrorInfo{
+			Reason:   err.Error(),
+			Domain:   puddingScheduler,
+			Metadata: map[string]string{"body": req.String()},
+		})
 	}
-	return &res.Response{}, nil
+	return &emptypb.Empty{}, nil
 }
 
 func (s *Handler) convPBToMessage(req *pb.SendDelayMessageRequest) *types.Message {
