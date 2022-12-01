a6ba9ef3d5baa7728de87f8a4dbd39a4f4b1aef0
beihai <beihai@wingsxdu.com>
2022-12-01 21:57:39 +0800

    feat: add cron trigger template ddl (#120)

--- app/trigger/repo/storage/po/cron_trigger_template_po.go
+++ app/trigger/repo/storage/po/cron_trigger_template_po.go
@@ -13,27 +13,29 @@ type CronTriggerTemplate struct {
 	gorm.Model
 
 	// CronExpr is the cron expression.
-	CronExpr string `gorm:"column:cron_expr;type:varchar(255);not null;default:'unknown';comment:'cron表达式'" copier:"must,nopanic"`
+	CronExpr string `gorm:"column:cron_expr;type:varchar(255);not null;default:'unknown';comment:'cron expr'" copier:"must,nopanic"`
 
 	// Topic the message topic
-	Topic string `gorm:"column:topic;type:varchar(255);not null;default:'unknown';comment:'消息Topic'" copier:"must,nopanic"`
+	Topic string `gorm:"column:topic;type:varchar(255);not null;default:'unknown';comment:'message topic'" copier:"must,nopanic"`
 	// Payload the message payload
-	Payload []byte `gorm:"column:payload;type:TEXT;not null;comment:'消息内容'" copier:"must,nopanic"`
+	Payload []byte `gorm:"column:payload;type:TEXT;not null;comment:'message content'" copier:"must,nopanic"`
 
 	// LastExecutionTime last time to schedule the message
-	LastExecutionTime time.Time `gorm:"column:last_execution_time;type:TIMESTAMP;not null;comment:'上次执行时间'" copier:"must,nopanic"`
+	LastExecutionTime time.Time `gorm:"column:last_execution_time;type:TIMESTAMP;not null;comment:'last time to schedule the message'" copier:"must,nopanic"`
 
-	// ExceptedEndTime Excepted Trigger end time, if it is 0, it means that it will not end.
-	ExceptedEndTime time.Time `gorm:"column:excepted_end_time;type:TIMESTAMP;not null;comment:'预期结束时间'" copier:"must,nopanic"`
+	// ExceptedEndTime excepted trigger end time, if it is 0, it means that it will not end.
+	ExceptedEndTime time.Time `gorm:"column:excepted_end_time;type:TIMESTAMP;not null;comment:'excepted trigger end time, if it is 0, it means that it will not end.'" copier:"must,nopanic"`
 	// ExceptedLoopTimes except loop times
-	ExceptedLoopTimes uint64 `gorm:"column:excepted_loop_times;type:int unsigned;not null;default:0;comment:'预期循环次数'" copier:"must,nopanic"`
+	ExceptedLoopTimes uint64 `gorm:"column:excepted_loop_times;type:int unsigned;not null;default:0;comment:'except loop times'" copier:"must,nopanic"`
 	// LoopedTimes already loop times
-	LoopedTimes uint64 `gorm:"column:looped_times;type:int unsigned;not null;default:0;comment:'已循环次数'" copier:"must,nopanic"`
+	LoopedTimes uint64 `gorm:"column:looped_times;type:int unsigned;not null;default:0;comment:'already loop times'" copier:"must,nopanic"`
 
-	// Status the CronTriggerTemplate status: enable disable offline
-	Status pb.TriggerStatus `gorm:"column:status;type:int unsigned;not null;default:0;comment:'CronTriggerTemplate status'" copier:"must,nopanic"`
+	// Status the trigger template status: enable->1 disable->2 offline->3 and so on
+	Status pb.TriggerStatus `gorm:"column:status;type:int unsigned;not null;default:0;comment:'trigger template status: enable->1 disable->2 offline->3 and so on'" copier:"must,nopanic"`
 }
 
+// TableName returns the table name of CronTriggerTemplate
+// impl gorm Tabler interface
 func (CronTriggerTemplate) TableName() string {
 	return "cron_trigger_template"
 }
