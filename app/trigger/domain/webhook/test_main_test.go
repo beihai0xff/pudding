0e4ce8e08eb4acfd286831ce0cf0025bb61d853d
beihai <beihai@wingsxdu.com>
2022-12-03 00:35:21 +0800

    feat: impl webhook trigger command

--- app/trigger/domain/webhook/test_main_test.go
+++ app/trigger/domain/webhook/test_main_test.go
@@ -1,19 +1,19 @@
-package repo
+package webhook
 
 import (
 	"os"
 	"testing"
+	"time"
 
+	"github.com/beihai0xff/pudding/app/trigger/repo"
 	"github.com/beihai0xff/pudding/app/trigger/repo/storage/po"
 	"github.com/beihai0xff/pudding/configs"
+	"github.com/beihai0xff/pudding/pkg/clock"
 	"github.com/beihai0xff/pudding/pkg/db/mysql"
 	"github.com/beihai0xff/pudding/pkg/log"
 )
 
-var (
-	testCronTemplate    CronTemplateDAO
-	testWebhookTemplate WebhookTemplate
-)
+var testTrigger *Trigger
 
 func TestMain(m *testing.M) {
 
@@ -21,7 +21,10 @@ func TestMain(m *testing.M) {
 	db := newMySQLClient()
 	createTable(db)
 
-	createDao(db)
+	testTrigger = &Trigger{
+		repo:      repo.NewWebhookTemplate(db),
+		wallClock: clock.NewFakeClock(time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC)),
+	}
 
 	code := m.Run()
 
@@ -39,24 +42,15 @@ func newMySQLClient() *mysql.Client {
 }
 
 func createTable(db *mysql.Client) {
-	err := db.Set("gorm:table_options", "ENGINE=InnoDB").Migrator().CreateTable(&po.CronTriggerTemplate{})
-	if err != nil {
-		log.Errorf("create table failed: %v", err)
-	}
-	err = db.Set("gorm:table_options", "ENGINE=InnoDB").Migrator().CreateTable(&po.WebhookTriggerTemplate{})
+	err := db.Set("gorm:table_options", "ENGINE=InnoDB").Migrator().CreateTable(&po.WebhookTriggerTemplate{})
 	if err != nil {
 		log.Errorf("create table failed: %v", err)
 	}
 }
 
 func dropTable(db *mysql.Client) {
-	err := db.Migrator().DropTable(&po.CronTriggerTemplate{})
+	err := db.Migrator().DropTable(&po.WebhookTriggerTemplate{})
 	if err != nil {
 		log.Errorf("drop table failed: %v", err)
 	}
-}
-
-func createDao(db *mysql.Client) {
-	testCronTemplate = NewCronTemplate(db)
-	testWebhookTemplate = NewWebhookTemplate(db)
 }
---
+++ app/trigger/domain/webhook/webhook_trigger_cmd_test.go
@@ -0,0 +1,159 @@
+package webhook
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	pb "github.com/beihai0xff/pudding/api/gen/pudding/trigger/v1"
+	"github.com/beihai0xff/pudding/app/trigger/entity"
+)
+
+func TestTrigger_Register(t1 *testing.T) {
+	type args struct {
+		ctx  context.Context
+		temp *entity.WebhookTriggerTemplate
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "normal",
+			args: args{
+				ctx: context.Background(),
+				temp: &entity.WebhookTriggerTemplate{
+					ID:                0,
+					Topic:             "test",
+					Payload:           []byte("hello"),
+					LoopedTimes:       0,
+					ExceptedLoopTimes: 10,
+				},
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			tt.wantErr(t1, testTrigger.Register(tt.args.ctx, tt.args.temp), fmt.Sprintf("Register(%+v, %+v)", tt.args.ctx, tt.args.temp))
+			e, err := testTrigger.repo.FindByID(tt.args.ctx, tt.args.temp.ID)
+			assert.NoError(t1, err)
+			assert.Equal(t1, tt.args.temp, e)
+		})
+	}
+}
+
+func TestTrigger_checkRegisterParams(t1 *testing.T) {
+	type args struct {
+		temp *entity.WebhookTriggerTemplate
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *entity.WebhookTriggerTemplate
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "normal",
+			args: args{
+				&entity.WebhookTriggerTemplate{
+					Topic:   "test",
+					Payload: []byte("hello"),
+				},
+			},
+			want: &entity.WebhookTriggerTemplate{
+				Topic:             "test",
+				Payload:           []byte("hello"),
+				ExceptedEndTime:   testTrigger.wallClock.Now().Add(entity.DefaultTemplateActiveDuration),
+				ExceptedLoopTimes: entity.DefaultMaximumLoopTimes,
+				Status:            pb.TriggerStatus_DISABLED,
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "topic not found",
+			args: args{
+				&entity.WebhookTriggerTemplate{
+					Payload: []byte("hello"),
+				},
+			},
+			want: &entity.WebhookTriggerTemplate{
+				Payload: []byte("hello"),
+			},
+			wantErr: assert.Error,
+		},
+		{
+			name: "payload not found",
+			args: args{
+				&entity.WebhookTriggerTemplate{
+					Topic: "test",
+				},
+			},
+			want: &entity.WebhookTriggerTemplate{
+				Topic: "test",
+			},
+			wantErr: assert.Error,
+		},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			err := testTrigger.checkRegisterParams(tt.args.temp)
+			tt.wantErr(t1, err, fmt.Errorf("checkRegisterParams got error (%w)", err))
+			assert.Equalf(t1, tt.want, tt.args.temp, fmt.Sprintf("checkRegisterParams(%+v)", tt.args.temp))
+		})
+	}
+}
+
+func TestTrigger_UpdateStatus(t1 *testing.T) {
+	temp := &entity.WebhookTriggerTemplate{
+		ID:                0,
+		Topic:             "test",
+		Payload:           []byte("hello"),
+		LoopedTimes:       0,
+		ExceptedEndTime:   testTrigger.wallClock.Now().AddDate(1, 1, 0),
+		ExceptedLoopTimes: 10,
+	}
+	ctx := context.Background()
+	testTrigger.Register(ctx, temp)
+
+	type args struct {
+		ctx    context.Context
+		id     uint
+		status pb.TriggerStatus
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "normal",
+			args: args{
+				ctx:    nil,
+				id:     temp.ID,
+				status: pb.TriggerStatus_ENABLED,
+			},
+			wantErr: assert.NoError,
+		},
+		{
+			name: "normal",
+			args: args{
+				ctx:    nil,
+				id:     temp.ID,
+				status: pb.TriggerStatus_DISABLED,
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			_, err := testTrigger.UpdateStatus(tt.args.ctx, tt.args.id, tt.args.status)
+			tt.wantErr(t1, err, fmt.Sprintf("UpdateStatus(%+v, %+v, %+v)", tt.args.ctx, tt.args.id, tt.args.status))
+			e, _ := testTrigger.repo.FindByID(ctx, tt.args.id)
+			assert.Equalf(t1, tt.args.status, e.Status, fmt.Sprintf("get UpdateStatus(%+v)", e))
+		})
+	}
+}
