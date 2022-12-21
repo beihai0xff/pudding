c3c17a3afdebf5ac9105b6da8e40419e8c8535ca
beihai <beihai@wingsxdu.com>
2022-12-21 23:56:15 +0800

    improve: support swagger ui

--- pkg/swagger/serve.go
+++ pkg/swagger/serve.go
@@ -16,17 +16,21 @@ import (
 // RegisterHandler register Swagger UI handler.
 func RegisterHandler(gwmux *runtime.ServeMux, prefix string) {
 	swaggerHandler := getOpenAPIHandler(third_party.Embed)
-	gwmux.HandlePath("GET", prefix+"/*", func(w http.ResponseWriter,
+	if err := gwmux.HandlePath("GET", prefix+"/*", func(w http.ResponseWriter,
 		r *http.Request, pathParams map[string]string) {
 		r.URL.Path = strings.TrimPrefix(r.URL.Path, prefix)
 		log.Infof("Serving swagger file: %s", r.URL.Path)
 		swaggerHandler.ServeHTTP(w, r)
-	})
+	}); err != nil {
+		log.Fatalf("failed to register swagger handler: %v", err)
+	}
 }
 
 // GetOpenAPIHandler serves an OpenAPI UI.
 func getOpenAPIHandler(efs embed.FS) http.Handler {
-	mime.AddExtensionType(".svg", "image/svg+xml")
+	if err := mime.AddExtensionType(".svg", "image/svg+xml"); err != nil {
+		log.Fatalf("failed to add mime type for .svg: %v", err)
+	}
 
 	// Use subdirectory in embedded files
 	subFS, err := fs.Sub(efs, "OpenAPI")
