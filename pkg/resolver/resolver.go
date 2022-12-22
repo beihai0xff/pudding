6ca40c6a565bd99f2d2013ba3d9b7480976e9938
beihai <beihai@wingsxdu.com>
2022-12-22 23:42:10 +0800

    improve: use grpc service desc ServiceName Register to Resolver (#150)
    * improve: use grpc service desc ServiceName Register to Resolver

--- pkg/resolver/resolver.go
+++ pkg/resolver/resolver.go
@@ -1,6 +1,22 @@
 package resolver
 
+import (
+	"github.com/beihai0xff/pudding/pkg/log"
+	"github.com/beihai0xff/pudding/pkg/utils"
+)
+
 type Resolver interface {
 	Register(serviceName string, ip string, port int) (string, error)
 	Deregister(serviceID string) error
+}
+
+type OptionResolver func() Resolver
+
+func GRPCRegistration(serviceName string, port int, opt OptionResolver) (Resolver, string) {
+	rsv := opt()
+	serviceID, err := rsv.Register(serviceName, utils.GetOutBoundIP(), port)
+	if err != nil {
+		log.Fatalf("failed to register service: %v", err)
+	}
+	return rsv, serviceID
 }
