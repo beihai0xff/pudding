7d4872a325b070ee8673105d9687a305c2193170
beihai <beihai@wingsxdu.com>
2022-12-24 23:42:45 +0800

    fix: handle http.ErrServerClosed error when graceful shutdown grpc gateway server

--- pkg/grpc/launcher/service.go
+++ pkg/grpc/launcher/service.go
@@ -104,6 +104,10 @@ func StartHTTPService(grpcLis, httpLis net.Listener, healthEndpointPath, swagger
 	go func() {
 		log.Infof("http server listening at %v", httpLis.Addr())
 		if err = httpServer.Serve(httpLis); err != nil {
+			if err == http.ErrServerClosed {
+				log.Info("http server closed")
+				return
+			}
 			log.Fatalf("Failed to serve gRPC-Gateway: %v", err)
 		}
 	}()
