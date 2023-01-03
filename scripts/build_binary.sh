b55dd5da71450da20c19240562c67313b1b738f2
beihai <beihai@wingsxdu.com>
2023-01-03 23:55:08 +0800

    improve: improve build scripts and app args name

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -8,7 +8,9 @@ binary_name="server"
 
 # output message
 echo "start build ${APP}"
+    mkdir -p ./build/bin/${APP}/certs/
     go build -v -o build/bin/${APP}/${binary_name} ./cmd/${APP}/
     cp ./build/config/${APP}.yaml ./build/bin/${APP}/config.yaml
+    cp ./build/bin/certs/pudding*.pem ./build/bin/${APP}/certs/
 
 echo "build ${APP} successfully"
