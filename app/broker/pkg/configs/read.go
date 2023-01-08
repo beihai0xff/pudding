e1b96a227fdb03e9e57c6d13e69b1e506d8ba4b1
beihai <beihai@wingsxdu.com>
2023-01-08 17:57:34 +0800

    feat: using generic refactoring set connector configuration capabilities

--- app/broker/pkg/configs/read.go
+++ app/broker/pkg/configs/read.go
@@ -5,25 +5,28 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/samber/lo"
+
 	conf "github.com/beihai0xff/pudding/configs"
 	"github.com/beihai0xff/pudding/types"
 )
 
-// JSON returns the json format of the config.
-func (c *Config) JSON() []byte {
-	b, err := json.Marshal(c)
-	if err != nil {
-		log.Panicf("marshal config failed: %v", err)
-		return nil
-	}
-
-	return b
-}
-
-func Init(filePath string, opts ...OptionFunc) {
-	conf.Parse(filePath, "yaml", c, conf.ReadFromFile)
+// ReadFrom read the config from the given configPath.
+func ReadFrom(configPath string, opts ...OptionFunc) {
+	conf.Parse(configPath, "yaml", c, conf.ReadFromFile)
 	c.ServerConfig.SetFlags()
 
+	for _, opt := range opts {
+		opt(c)
+	}
+	setConnector()
+
+	var str bytes.Buffer
+	_ = json.Indent(&str, c.JSON(), "", "    ")
+	log.Printf("pudding scheduler config:\n %s \n", str.String())
+}
+
+func setConnector() {
 	if c.ServerConfig.MessageTopic == "" {
 		c.ServerConfig.MessageTopic = types.DefaultTopic
 	}
@@ -31,42 +34,33 @@ func Init(filePath string, opts ...OptionFunc) {
 		c.ServerConfig.TokenTopic = types.TokenTopic
 	}
 
-	for _, opt := range opts {
-		opt(c)
-	}
-
-	producers := make(map[string]struct{})
-	for _, v := range c.Pulsar.ProducersConfig {
-		producers[v.Topic] = struct{}{}
-	}
-
-	if _, ok := producers[c.ServerConfig.MessageTopic]; !ok {
+	if !lo.ContainsBy[conf.ProducerConfig](c.Pulsar.ProducersConfig, func(v conf.ProducerConfig) bool {
+		return v.Topic == c.ServerConfig.MessageTopic
+	}) {
 		c.Pulsar.ProducersConfig = append(c.Pulsar.ProducersConfig, conf.ProducerConfig{
-			Topic:                   types.DefaultTopic,
+			Topic:                   c.ServerConfig.MessageTopic,
 			BatchingMaxPublishDelay: 20,
 			BatchingMaxMessages:     100,
 			BatchingMaxSize:         1024,
 		})
 	}
 
-	if _, ok := producers[c.ServerConfig.TokenTopic]; !ok {
+	if !lo.ContainsBy[conf.ProducerConfig](c.Pulsar.ProducersConfig, func(v conf.ProducerConfig) bool {
+		return v.Topic == c.ServerConfig.TokenTopic
+	}) {
 		c.Pulsar.ProducersConfig = append(c.Pulsar.ProducersConfig, conf.ProducerConfig{
-			Topic:                   types.TokenTopic,
+			Topic:                   c.ServerConfig.TokenTopic,
 			BatchingMaxPublishDelay: 20,
 			BatchingMaxMessages:     100,
 			BatchingMaxSize:         1024,
 		})
 	}
-
-	var str bytes.Buffer
-	_ = json.Indent(&str, c.JSON(), "", "    ")
-	log.Printf("pudding scheduler config:\n %s \n", str.String())
 }
 
 // OptionFunc is the option function for config.
 type OptionFunc func(config *Config)
 
-// WithRedisURL sets the redis url.
+// WithRedisURL set the redis url.
 func WithRedisURL(url string) OptionFunc {
 	return func(config *Config) {
 		if url != "" {
@@ -75,7 +69,7 @@ func WithRedisURL(url string) OptionFunc {
 	}
 }
 
-// WithPulsarURL sets the pulsar url.
+// WithPulsarURL set the pulsar url.
 func WithPulsarURL(url string) OptionFunc {
 	return func(config *Config) {
 		if url != "" {
