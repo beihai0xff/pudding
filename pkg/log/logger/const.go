7fac396fe856f204f18a2959546f4ed3b42b5899
beihai <beihai@wingsxdu.com>
2023-01-10 00:17:07 +0800

    improve: logger add package comment and exported method comment (#182)
    * improve: logger add package comment and exported method comment

--- pkg/log/logger/const.go
+++ pkg/log/logger/const.go
@@ -1,7 +1,11 @@
+// Package logger defines logging for components in the pudding project.
 package logger
 
 const (
-	PulsarLoggerName  = "pulsar_log"
-	GRPCLoggerName    = "grpc_log"
+	// PulsarLoggerName is the name of the logger for pulsar.
+	PulsarLoggerName = "pulsar_log"
+	// GRPCLoggerName is the name of the logger for grpc.
+	GRPCLoggerName = "grpc_log"
+	// BackendLoggerName is the name of the logger for gorm.
 	BackendLoggerName = "backend_log"
 )
--- pkg/log/logger/pulsar_logger.go
+++ pkg/log/logger/pulsar_logger.go
@@ -1,3 +1,5 @@
+// Package logger defines logging for components in the pudding project.
+// pulsar_logger.go defines logging for pulsar.
 package logger
 
 import (
@@ -6,11 +8,13 @@ import (
 	"github.com/beihai0xff/pudding/pkg/log"
 )
 
+// PulsarLogger is a wrapper of log.Logger to implement pulsar.Logger.
 type PulsarLogger struct {
 	log.Logger
 	with func(fields ...interface{}) log.Logger
 }
 
+// SubLogger returns a sub logger with the given Fields.
 func (p *PulsarLogger) SubLogger(fields plog.Fields) plog.Logger {
 	f := make([]interface{}, 0, 2*len(fields))
 
@@ -20,6 +24,7 @@ func (p *PulsarLogger) SubLogger(fields plog.Fields) plog.Logger {
 	return &PulsarLogger{Logger: p.with(f...), with: p.with}
 }
 
+// WithFields returns a new Entry with the fields added to it.
 func (p *PulsarLogger) WithFields(fields plog.Fields) plog.Entry {
 	f := make([]interface{}, 0, 2*len(fields))
 
@@ -30,14 +35,17 @@ func (p *PulsarLogger) WithFields(fields plog.Fields) plog.Entry {
 	return &PulsarLogger{Logger: p.with(f...), with: p.with}
 }
 
+// WithField returns a new Entry with the field added to it.
 func (p *PulsarLogger) WithField(name string, value interface{}) plog.Entry {
 	return &PulsarLogger{Logger: p.with(name, value), with: p.with}
 }
 
+// WithError returns a new Entry with the field added to it.
 func (p *PulsarLogger) WithError(err error) plog.Entry {
 	return &PulsarLogger{Logger: p.with("error", err), with: p.with}
 }
 
+// GetPulsarLogger returns a pulsar.Logger that uses the given pudding logger.
 func GetPulsarLogger() plog.Logger {
 	l := log.GetLoggerByName(PulsarLoggerName).WithFields("module", "pulsar")
 	return &PulsarLogger{Logger: l, with: l.WithFields}
