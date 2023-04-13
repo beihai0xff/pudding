007cb3b8c3dbc89794e72a0d808b376d228c7e3a
beihai <beihai@wingsxdu.com>
2023-04-14 00:05:35 +0800

    improve: easy way use docker compose to deploy

--- scripts/deploy/deploy_docker_compose.sh
+++ scripts/deploy/deploy_docker_compose.sh
@@ -2,8 +2,12 @@
 
 set -ex
 
+echo "starting infra and prom containers..."
 ADMIN_USER='admin' ADMIN_PASSWORD='admin' ADMIN_PASSWORD_HASH='$2a$14$1l.IozJx7xQRVmlkEQ32OeEEfP5mRxTpbDTCTcXRqn19gXD8YK1pO' \
 	docker compose -f ./deployments/docker-compose/dockprom/docker-compose.yml \
 	-f ./deployments/docker-compose/infra.yml \
-	-f ./deployments/docker-compose/pudding.yml \
-	-p pudding up --always-recreate-deps -V
+	-p pudding-infra up --force-recreate --wait --wait-timeout 120
+
+echo "infra and prom containers start successfully"
+echo "start pudding containers..."
+docker compose -p pudding-service -f ./deployments/docker-compose/pudding.yml up --force-recreate -V --abort-on-container-exit
