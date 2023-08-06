37d08ba8c81ce34610c0944548b94542e3dd2c14
beihai <beihai0xff@gmail.com>
2023-08-07 00:21:18 +0800

    ci: improve ci scripts (#343)
    * ci: improve ci scripts

--- scripts/gen_configs_struct_tag.sh
+++ scripts/gen_configs_struct_tag.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-set -e -x
+set -ex
 
-gomodifytags -all -add-tags json,yaml,mapstructure -w --quiet\
-   -file configs/kafka_config.go \
-   -file configs/log_config.go \
-   -file configs/mysql_config.go \
-   -file configs/pulsar_config.go \
-   -file configs/redis_config.go \
-   -file configs/server_base_config.go \
-   -file configs/server_broker_config.go \
-   -file configs/server_trigger_config.go \
-
-
+gomodifytags -all -add-tags json,yaml,mapstructure -w --quiet -file configs/kafka_config.go \
+	-file configs/log_config.go \
+	-file configs/mysql_config.go \
+	-file configs/pulsar_config.go \
+	-file configs/redis_config.go \
+	-file configs/server_base_config.go \
+	-file configs/server_broker_config.go \
+	-file configs/server_trigger_config.go
--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-set -e -x
+set -e x
 
 make clean
 make bootstrap
-make gen/mock
-make gen/struct_tag
 make env/mysql
 go test -v -race -covermode=atomic -coverprofile=coverprofile.cov ./...
 go tool cover -func=coverprofile.cov
