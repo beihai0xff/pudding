6fe1bbbf9ae651d770848576ffdb7cb32d0a477e
beihai <beihai0xff@gmail.com>
2023-08-18 23:09:11 +0800

    feat: support redis connector (#358)

--- app/broker/connector/redis_connector/redis_connector.go
+++ app/broker/connector/redis_connector/redis_connector.go
@@ -3,7 +3,6 @@ package redis_connector
 
 import (
 	"context"
-	"time"
 
 	"github.com/redis/go-redis/v9"
 
@@ -44,28 +43,25 @@ func (q *Connector) Produce(ctx context.Context, msg *types.Message) error {
 func (q *Connector) NewConsumer(topic, group string, batchSize int, fn type2.HandleMessage) error {
 	go func() {
 		for {
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			// 拉取已经投递却未被 ACK 的消息，保证消息至少被成功消费1次
+			ctx := context.Background()
+			// pull unack message, ensure message is consumed at least once
 			msgs, err := q.rdb.XGroupConsume(ctx, topic, group, q.consumerName, "0", batchSize)
 			if err != nil {
 				log.Errorf("XGroupConsume unack message error: %v", err)
 			}
 
 			if len(msgs) == batchSize {
-				// 如果一次未拉取完未被 ACK 的消息，则继续拉取
-				// 确保先消费完成未被 ACK 的消息
+				// if unack message is not consumed, continue to consume
 				continue
 			}
 
-			// 拉取新消息
+			// pull new messages
 			msgs, err = q.rdb.XGroupConsume(ctx, topic, group, q.consumerName, ">", batchSize)
 			if err != nil {
 				log.Errorf("XGroupConsume message error: %v", err)
 			}
 
 			q.handlerRealTimeMessage(ctx, msgs, topic, group, fn)
-
-			cancel()
 		}
 	}()
 
@@ -75,25 +71,22 @@ func (q *Connector) NewConsumer(topic, group string, batchSize int, fn type2.Han
 // handlerRealTimeMessage handle Messages from the queue in real time
 func (q *Connector) handlerRealTimeMessage(ctx context.Context, msgs []redis.XMessage, topic, group string,
 	fn type2.HandleMessage) {
-	// 遍历处理消息
 	for _, msg := range msgs {
 		var m *types.Message
-		// TODO: 消费超过三次的消息，记录错误日志，并添加到死信队列
-		err := msgpack.Decode(msg.Values["body"].([]byte), m)
-		if err != nil {
-			// TODO: 记录错误日志
+		if err := msgpack.Decode(msg.Values["body"].([]byte), m); err != nil {
+			log.Errorf("decode message error: %v", err)
 			continue
 		}
 
 		// handle message
 		if err := fn(ctx, m); err != nil {
-			// TODO: 记录错误日志
+			log.Errorf("handle message error: %v", err)
 			continue
 		}
 
 		// handle message success，ACK
 		if err := q.rdb.XAck(ctx, topic, group, msg.ID); err != nil {
-			// TODO: 记录错误日志
+			log.Errorf("XAck message error: %v", err)
 			continue
 		}
 	}
