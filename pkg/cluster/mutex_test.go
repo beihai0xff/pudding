e878edebc1a64ab4a6a0627b35028bb24fecae70
beihai <beihai0xff@gmail.com>
2023-09-15 00:24:39 +0800

    feat: improve Mutex IsLocked unittest (#374)
    * feat: improve Mutex IsLocked unittest

--- pkg/cluster/mutex_test.go
+++ pkg/cluster/mutex_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Test_mutex_Lock(t *testing.T) {
-	mutex, err := testCluster.Mutex("test", 2*time.Second, WithDisableKeepalive())
+	mutex, err := testCluster.Mutex("test", 2*time.Second)
 	assert.NoError(t, err)
 
 	ctx := context.Background()
@@ -21,6 +21,18 @@ func Test_mutex_Lock(t *testing.T) {
 	assert.NoError(t, mutex.Lock(ctx))
 	assert.ErrorIs(t, ErrLockedBySelf, mutex.Lock(ctx))
 	assert.NoError(t, mutex.Unlock(ctx))
+}
+
+func Test_mutexUnlock(t *testing.T) {
+	mutex, err := testCluster.Mutex("test/unlock", 2*time.Second)
+	assert.NoError(t, err)
+
+	ctx := context.Background()
+	assert.NoError(t, mutex.Lock(ctx))
+	assert.NoError(t, mutex.Unlock(ctx))
+	// unlock again
+	assert.ErrorIs(t, mutex.Unlock(ctx), ErrLockNotHeld)
+	assert.ErrorIs(t, mutex.Unlock(ctx), ErrLockNotHeld)
 }
 
 func Test_mutex_Refresh(t *testing.T) {
@@ -57,16 +69,23 @@ func refreshMutexAfter(t *testing.T, m Mutex, d time.Duration) {
 }
 
 func Test_mutex_IsLocked(t *testing.T) {
-	mutex, err := testCluster.Mutex("test", 2*time.Second)
+	mutex1, err := testCluster.Mutex("test", 2*time.Second)
 	assert.NoError(t, err)
 	mutex2, err := testCluster.Mutex("test", 2*time.Second)
 	assert.NoError(t, err)
 
 	ctx := context.Background()
-	assert.NoError(t, mutex.Lock(ctx))
-	assert.Equal(t, true, mutex.IsLocked())
+	assert.NoError(t, mutex1.Lock(ctx))
+	assert.Equal(t, true, mutex1.IsLocked())
 	assert.Equal(t, false, mutex2.IsLocked())
 
-	assert.NoError(t, mutex.Unlock(ctx))
-	assert.Equal(t, false, mutex.IsLocked())
+	assert.NoError(t, mutex1.Unlock(ctx))
+	assert.Equal(t, false, mutex1.IsLocked())
+
+	assert.NoError(t, mutex2.Lock(ctx))
+	assert.Equal(t, true, mutex2.IsLocked())
+
+	mutex3 := mutex2.(*mutex)
+	assert.NoError(t, mutex3.session.Close())
+	assert.Equal(t, false, mutex2.IsLocked())
 }
